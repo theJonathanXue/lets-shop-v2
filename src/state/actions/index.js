f4a4caf945a7dea9d09b51e98243af9d194a43ad
j52 <j52xue@uwaterloo.ca>
2022-09-12 19:27:35 -0400

    Updates

--- src/state/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { openCart, closeCart } from './isCartOpen'
-import { addToCart, removeFromCart } from './cart'
-import { setProducts } from './products'
-
-export { openCart, closeCart, addToCart, removeFromCart, setProducts }
